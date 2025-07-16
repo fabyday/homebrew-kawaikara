71a1e7527e9c95a75264a0e44995b08ca31bb9c3
yabai <rohjihyun95@gmail.com>
2025-07-17 01:16:26 +0900

    fix mistyping

--- Casks/kawaikara.rb
+++ Casks/kawaikara.rb
@@ -1,8 +1,8 @@
 cask "kawaikara" do 
     version "2.2.0"
     sha256 "3a1b7df42d39147cdb27e2968f753fec30b7a75a54bf937459eb737e997cc2ee"
-    url "https://github.com/fabyday/kawaikara/releases/download/v2.2.0/kawaikara-2.2.0-arm64.zip"
-          verified: "github.com/fabyday/kawaikara/"
+    url "https://github.com/fabyday/kawaikara/releases/download/v2.2.0/kawaikara-2.2.0-arm64.zip",
+        verified: "github.com/fabyday/kawaikara/"
 
     name  "kawaikara"
     desc "Kawaiii OTT viewer"
