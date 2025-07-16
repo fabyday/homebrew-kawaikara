0347d7c10f372462383f1e3b93d84551ce257390
yabai <rohjihyun95@gmail.com>
2025-07-17 02:23:54 +0900

    add intel version app

--- Casks/kawaikara.rb
+++ Casks/kawaikara.rb
@@ -1,8 +1,17 @@
 cask "kawaikara" do 
     version "2.2.0"
-    sha256 "3a1b7df42d39147cdb27e2968f753fec30b7a75a54bf937459eb737e997cc2ee"
-    url "https://github.com/fabyday/kawaikara/releases/download/v2.2.0/kawaikara-2.2.0-arm64-mac.zip",
-        verified: "github.com/fabyday/kawaikara/"
+    
+    on_arm do 
+        sha256 "3a1b7df42d39147cdb27e2968f753fec30b7a75a54bf937459eb737e997cc2ee"
+        url "https://github.com/fabyday/kawaikara/releases/download/v2.2.0/kawaikara-2.2.0-arm64-mac.zip",
+            verified: "github.com/fabyday/kawaikara/"
+    end
+    
+    on_intel do 
+        sha256 "ac66b2a06863faf044a05ab89bc5cb46a441935f907094cee89887e5d9d51797"
+        url "https://github.com/fabyday/kawaikara/releases/download/v2.2.0/kawaikara-2.2.0-mac.zip",
+            verified: "github.com/fabyday/kawaikara/"
+    end 
 
     name  "kawaikara"
     desc "Kawaiii OTT viewer"
@@ -13,4 +22,6 @@ cask "kawaikara" do
     "~/Library/Application Support/Kawaikara",
     "~/Library/Preferences/com.kawaikara.*",
         ]
+
+
 end
