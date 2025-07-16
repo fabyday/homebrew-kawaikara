98712e529ba59a06042aa067cdaea87f465fa5c5
yabai <rohjihyun95@gmail.com>
2025-07-17 01:13:11 +0900

    init homebrew repo

---
+++ Casks/kawaikara.rb
@@ -0,0 +1,16 @@
+cask "kawaikara" do 
+    version "2.2.0"
+    sha256 "3a1b7df42d39147cdb27e2968f753fec30b7a75a54bf937459eb737e997cc2ee"
+    url "https://github.com/fabyday/kawaikara/releases/download/v2.2.0/kawaikara-2.2.0-arm64.zip"
+          verified: "github.com/fabyday/kawaikara/"
+
+    name  "kawaikara"
+    desc "Kawaiii OTT viewer"
+    
+    homepage "https://github.com/fabyday/kawaikara"
+    app "kawaikara.app"
+    zap trash: [
+    "~/Library/Application Support/Kawaikara",
+    "~/Library/Preferences/com.kawaikara.*",
+        ]
+end
